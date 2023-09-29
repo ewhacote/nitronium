99d71882c211248659d3777ca2653ad40935d228
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-09-29 22:33:40 +0900

    [SQL] 중복 제거하기 / https://school.programmers.co.kr/learn/courses/30/lessons/59408

---
+++ sql/59408_중복 제거하기.cpp
@@ -0,0 +1,8 @@
+//
+// Created by KangMinji on 2023-09-29.
+// https://school.programmers.co.kr/learn/courses/30/lessons/59408
+
+-- 코드를 입력하세요
+SELECT COUNT(DISTINCT(NAME)) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
