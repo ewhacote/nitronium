e4fc34f8c9c948ab37b1944413c0f1f7881e831d
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-02 23:43:29 +0900

    [SQL] 이름에 el 들어가는 동물 찾기 / https://school.programmers.co.kr/learn/courses/30/lessons/59047

---
+++ sql/59047_이름에 el.cpp
@@ -0,0 +1,9 @@
+//
+// Created by KangMinji on 2023-10-02.
+// https://school.programmers.co.kr/learn/courses/30/lessons/59047
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%el%' AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
