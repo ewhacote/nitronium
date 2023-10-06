cae02ca874e69a897496fa91485e09942ccef64f
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-10-06 22:52:36 +0900

    [SQL] 고양이와 개는 몇 마리 있을까 / https://school.programmers.co.kr/learn/courses/30/lessons/59040

---
+++ sql/59040_고양이와 개는 몇 마리 있을까.cpp
@@ -0,0 +1,9 @@
+//
+// Created by KangMinji on 2023-10-06.
+// https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+-- 코드를 입력하세요
+SELECT ANIMAL_TYPE, COUNT(*) AS COUNT
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
